70a6a49860dbc615d17cfdb0ad823481a7b63e34
momoko512 <momoko512@163.com>
2018-08-09 15:24:35 +0800

    threegit --ament

--- src/main/java/DayDayUp.java
+++ src/main/java/DayDayUp.java
@@ -5,6 +5,6 @@ public class DayDayUp {
     public static void main(String[] args) {
         System.out.println("syudyddddd");
         System.out.println();
-        System.out.println();
+        System.out.println("");
     }
 }
