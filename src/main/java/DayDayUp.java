a4e60e79b2bf39e1b5f9a04e14fe99a7f42753b0
momoko512 <momoko512@163.com>
2018-08-09 15:09:48 +0800

    twogit

--- src/main/java/DayDayUp.java
+++ src/main/java/DayDayUp.java
@@ -4,5 +4,7 @@
 public class DayDayUp {
     public static void main(String[] args) {
         System.out.println("syudyddddd");
+        System.out.println();
+        System.out.println();
     }
 }
